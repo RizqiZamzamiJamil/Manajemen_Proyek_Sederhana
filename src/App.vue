50e1bb7b4d840a12e78de62888807f8b923f2dbf
RizqiZamzamiJamil <rizqizamzamij@gmail.com>
2024-05-02 11:11:55 +0700

    Commit Project

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container mt-5">
+    <h1>Manajemen Proyek</h1>
+
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#tambahProyek">Tambah <i class="fa-solid fa-plus"></i></button>
+
+    <Main />
+
+  </div>
+</template>
+
+<script>
+import Main from './components/Main.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Main,
+  },
+  data() {
+    return {
+      projects: [],
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
